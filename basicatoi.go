feb5ba65573a704a0097743187380c1961a6d02e
01zombie <xya.messioui@gmail.com>
2024-02-11 19:06:12 +0100

    777

--- basicatoi.go
+++ basicatoi.go
@@ -4,11 +4,10 @@ func BasicAtoi(s string) int {
 	result := 0
 
 	for _, v := range s {
-		switch {
-		case v >= '0' && v <= '9':
-			result = result*10 + int(v-'0')
 
-		}
+		result = result*10 + int(v-'0')
+
 	}
+
 	return result
 }
